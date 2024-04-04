3add44ce2532adbae6f3b5fb2c0d0225f2ce3073
Max Luebke <mluebke@gfz-potsdam.de>
2024-04-04 09:27:27 +0000

    Add functions for checking sign and negativity in dol.pqi and dolo_200.R

--- bench/dolo/het/dolo_200.R
+++ bench/dolo/het/dolo_200.R
@@ -53,6 +53,43 @@ fuzz_input_dht_keys <- function(input) {
     return(input[names(dht_species)])
 }
 
+check_sign_cal_dol_interp <- function(to_interp, data_set) {
+    dht_species <- c(
+        "H" = 3,
+        "O" = 3,
+        "Charge" = 3,
+        "C(4)" = 6,
+        "Ca" = 6,
+        "Cl" = 3,
+        "Mg" = 5,
+        "Calcite" = 4,
+        "Dolomite" = 4
+    )
+    data_set <- as.data.frame(do.call(rbind, data_set), check.names = FALSE, optional = TRUE)
+    names(data_set) <- names(dht_species)
+    cal <- (data_set$Calcite == 0) == (to_interp["Calcite"] == 0)
+    dol <- (data_set$Dolomite == 0) == (to_interp["Dolomite"] == 0)
+
+    cal_dol_same_sig <- cal == dol
+    return(rev(which(!cal_dol_same_sig)))
+}
+
+check_neg_cal_dol <- function(result) {
+    neg_sign <- (result["Calcite"] < 0) || (result["Dolomite"] < 0)
+    return(neg_sign)
+}
+
+# Optional when using Interpolation (example with less key species and custom
+# significant digits)
+
+pht_species <- c(
+    "C(4)" = 3,
+    "Ca" = 3,
+    "Mg" = 2,
+    "Calcite" = 2,
+    "Dolomite" = 2
+)
+
 chemistry_setup <- list(
     dht_species = c(
         "H" = 3,
@@ -65,9 +102,12 @@ chemistry_setup <- list(
         "Calcite" = 4,
         "Dolomite" = 4
     ),
+    pht_species = pht_species,
     hooks = list(
         dht_fill = check_sign_cal_dol_dht,
-        dht_fuzz = fuzz_input_dht_keys
+        dht_fuzz = fuzz_input_dht_keys,
+        interp_pre = check_sign_cal_dol_interp,
+        interp_post = check_neg_cal_dol
     )
 )
 
