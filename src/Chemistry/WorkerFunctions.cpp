692c47781ed1bf901010dce165ccc69dae09eb98
hans <hans@straile.net>
2024-05-31 11:45:19 +0200

    fix: wp_start_index in Worker

--- src/Chemistry/WorkerFunctions.cpp
+++ src/Chemistry/WorkerFunctions.cpp
@@ -152,8 +152,8 @@ void poet::ChemistryModule::WorkerDoWork(MPI_Status &probe_status,
   // current simulation time ('age' of simulation)
   current_sim_time = mpi_buffer[count + 3];
 
-  /* 4th double value is currently a placeholder */
-  // placeholder = mpi_buffer[count+4];
+  // current work package start location in field
+  wp_start_index = mpi_buffer[count + 4];
 
   for (std::size_t wp_i = 0; wp_i < s_curr_wp.size; wp_i++) {
     s_curr_wp.input[wp_i] =
