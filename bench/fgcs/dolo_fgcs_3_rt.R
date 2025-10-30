5ee9f3273ceade57fef47b88390e9cdd80400247
Max Lübke <mluebke@uni-potsdam.de>
2025-10-30 08:29:57 +0100

    feat: Add initial configuration files for FGCS simulation

---
+++ bench/fgcs/dolo_fgcs_3_rt.R
@@ -0,0 +1,8 @@
+iterations <- 10000
+dt <- 200
+
+list(
+    timesteps = rep(dt, iterations),
+    store_result = TRUE,
+    out_save = out_save = c(1,20,50, seq(100,1000, by=100), seq(2000, iterations, by=1000))
+)
