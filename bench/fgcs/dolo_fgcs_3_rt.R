2f1cb6d4c115e3f60ee1ddc99074b1e3d516acad
Max Lübke <mluebke@uni-potsdam.de>
2025-10-30 08:53:38 +0100

    fix: Correct duplicate assignment in out_save variable

--- bench/fgcs/dolo_fgcs_3_rt.R
+++ bench/fgcs/dolo_fgcs_3_rt.R
@@ -4,5 +4,5 @@ dt <- 200
 list(
     timesteps = rep(dt, iterations),
     store_result = TRUE,
-    out_save = out_save = c(1,20,50, seq(100,1000, by=100), seq(2000, iterations, by=1000))
+    out_save = c(1,20,50, seq(100,1000, by=100), seq(2000, iterations, by=1000))
 )
