b17eaf777873f9c446bc4bf9e38d5d950c752806
rastogi <rastogi@hpc-login.hpc.cs.uni-potsdam.de>
2025-11-10 11:04:08 +0100

    Untrack share/ directory

--- share/poet/barite/barite_200_rt.R
+++
@@ -1,7 +0,0 @@
-iterations <- 50
-dt <- 100
-
-list(
-    timesteps = rep(dt, iterations),
-    store_result = TRUE
-)
--- share/poet/barite/barite_het_rt.R
+++
@@ -1,4 +0,0 @@
-list(
-    timesteps = rep(50, 100),
-    store_result = TRUE
-)
