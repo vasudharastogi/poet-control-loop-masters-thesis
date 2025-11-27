fddef8d01d9b737a5e77a35d6fc3242c6d48727e
rastogi <rastogi@hpc-login.hpc.cs.uni-potsdam.de>
2025-11-27 11:56:55 +0100

    Fix control_batch naming, metrics workflow and add R visualization script

--- bin/run_poet.sh
+++ bin/run_poet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=proto2_eps01_3_rb
-#SBATCH --output=proto2_eps01_3_rb_%j.out
-#SBATCH --error=proto2_eps01_3_rb_%j.err
+#SBATCH --job-name=proto2_eps0035_no_rb_v2
+#SBATCH --output=proto2_eps0035_no_rb_v2_%j.out
+#SBATCH --error=proto2_eps0035_no_rb_v2%j.err
 #SBATCH --partition=long
 #SBATCH --nodes=6                                                    
 #SBATCH --ntasks-per-node=24 
@@ -15,5 +15,5 @@ module purge
 module load cmake gcc openmpi
 
 #mpirun -n 144 ./poet dolo_fgcs_3.R dolo_fgcs_3.qs2 dolo_only_pqc
-mpirun -n 144 ./poet --interp dolo_fgcs_3_rt.R dolo_fgcs_3.qs2 proto2_eps01_3_rb
+mpirun -n 144 ./poet --interp --rds dolo_fgcs_3_rt.R dolo_fgcs_3.qs2 proto2_eps0035_no_rb_v2
 #mpirun -n 144 ./poet --interp  barite_fgcs_4_new/barite_fgcs_4_new_rt.R barite_fgcs_4_new/barite_fgcs_4_new.qs2 barite
