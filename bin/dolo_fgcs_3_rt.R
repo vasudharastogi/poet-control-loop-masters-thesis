375d26e5e8773c3c29adda18302060f5f0b6a233
rastogi <rastogi@hpc-login.hpc.cs.uni-potsdam.de>
2026-01-19 12:24:24 +0100

    Updates runtime parameters

--- bin/dolo_fgcs_3_rt.R
+++ bin/dolo_fgcs_3_rt.R
@@ -1,10 +1,11 @@
 iterations <- 10000
 dt <- 200
-ctrl_interval <- 200
-mape_threshold <- rep(7e-3, 13)
+ctrl_interval <- 50
+chkpt_interval <- ctrl_interval
+mape_threshold <- rep(1.75e-3, 13)
 mape_threshold[5] <- 1 #Charge
 zero_abs <- 1e-13
-rb_limit <- 0
+rb_limit <- 3
 rb_aging_limit <- 2*ctrl_interval
 
 #ctrl_cell_ids <- seq(0, (400*400)/2 - 1, by = 401)
@@ -16,8 +17,8 @@ list(
     timesteps = rep(dt, iterations),
     store_result = TRUE,
     out_save = out_save,
-    chkpt_interval = ctrl_interval,
     ctrl_interval = ctrl_interval,
+    chkpt_interval = chkpt_interval,
     mape_threshold = mape_threshold,
     zero_abs = zero_abs,
     rb_limit = rb_limit,
